6a270ceec611888738c31b164cd0f3a76fdeceea
amiraber <155098066+amiraber@users.noreply.github.com>
2025-04-04 16:08:24 +0200

    classes controllers

---
+++ src/main/java/Controller/AdminController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import Model.Admin;
+import Service.AdminService;
+
+public class AdminController {
+	
+	private AdminService adminService;
+
+	public AdminController() {
+	    this.adminService = new AdminService();
+	}
+	
+	public boolean adminExist(int matricule) {
+	    return adminService.adminExist(matricule);
+	}
+
+	public boolean ajouterAdmin(Admin admin) {
+	    return adminService.ajouterAdmin(admin);
+	}
+
+	public Admin getAdminByMat(int matricule) {
+	    return adminService.getAdminByMat(matricule);
+	}
+
+	public boolean modifierAdmin(Admin admin) {
+	    return adminService.modifierAdmin(admin);
+	}
+
+	public boolean supprimerAdmin(int matricule) {
+	    return adminService.supprimerAdmin(matricule);
+	}
+}
--- src/main/java/Controller/EtudiantController.java
+++ src/main/java/Controller/EtudiantController.java
@@ -12,9 +12,34 @@ public class EtudiantController {
 		this.etudiantService = new EtudiantService();
 	}
 	
+	public boolean etudiantExist(int matricule) {
+	    return etudiantService.etudiantExist(matricule);
+	}
+	
 	public void ajouterEtudiant(Etudiant etudiant) {
 		etudiantService.ajouterEtudiant(etudiant);
 	}
+	
+	public Etudiant getEtudiantByMat(int matricule) {
+	    return etudiantService.getEtudiantByMat(matricule);
+	}
+
+	public List<Etudiant> getEtudiantByNom(String nom) {
+	    return etudiantService.getEtudiantByNom(nom);
+	}
+
+	public List<Etudiant> getEtudiantBySpecialite(String specialite) {
+	    return etudiantService.getEtudiantBySpecialite(specialite);
+	}
+
+	public List<Etudiant> getEtudiantBySection(String specialite, String section) {
+	    return etudiantService.getEtudiantBySection(specialite, section);
+	}
+
+	public List<Etudiant> getEtudiantByGroupe(String specialite, String section, String groupe) {
+	    return etudiantService.getEtudiantByGroupe(specialite, section, groupe);
+	}
+
 	
 	public void modifierEtudiant(Etudiant etudiant) {
 		etudiantService.modifierEtudiant(etudiant);
@@ -22,10 +47,6 @@ public class EtudiantController {
 	
 	public void supprimerEtudiant(int idEtudiant) {
 		etudiantService.supprimerEtudiant(idEtudiant);
-	}
-	
-	public Etudiant getEtudiant(int idEtudiant) {
-		return etudiantService.getEtudiant(idEtudiant);
 	}
 	
 	public List<Etudiant> getAllEtudiands(){
