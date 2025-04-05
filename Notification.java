251655d2b97e2eb496b98c39b1b5a7bf3509a89d
Sarahougour <155068819+Sarahougour@users.noreply.github.com>
2025-04-05 13:36:45 +0100

    Add files via upload
    la classe de notification (dans la méthode ajoute Notification j'ai pas vérifiée est ce que cette notification a déjà existe "même destinataire même texte et même Expéditeur " car en peut faire des rappels )

---
+++ Notification.java
@@ -0,0 +1,91 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Notification {
+	 private int idNotification;
+	    private String message;
+	    private Timestamp dateEnvoi;  
+	    private boolean lu;  
+	    private TypeNotification typeNotification;
+	    private int idExpediteur;
+	    private int idDestinataire;
+		
+	    public Notification(int idNotification, String message, Timestamp dateEnvoi, boolean lu,
+				TypeNotification typeNotification, int idExpediteur, int idDestinataire) {
+			super();
+			this.idNotification = idNotification;
+			this.message = message;
+			this.dateEnvoi = dateEnvoi;
+			this.lu = lu;
+			this.typeNotification = typeNotification;
+			this.idExpediteur = idExpediteur;
+			this.idDestinataire = idDestinataire;
+		}
+
+		public int getIdNotification() {
+			return idNotification;
+		}
+
+		public void setIdNotification(int idNotification) {
+			this.idNotification = idNotification;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		public Timestamp getDateEnvoi() {
+			return dateEnvoi;
+		}
+
+		public void setDateEnvoi(Timestamp dateEnvoi) {
+			this.dateEnvoi = dateEnvoi;
+		}
+
+		public boolean isLu() {
+			return lu;
+		}
+
+		public void setLu(boolean lu) {
+			this.lu = lu;
+		}
+
+		public TypeNotification getTypeNotification() {
+			return typeNotification;
+		}
+
+		public void setTypeNotification(TypeNotification typeNotification) {
+			this.typeNotification = typeNotification;
+		}
+
+		public int getIdExpediteur() {
+			return idExpediteur;
+		}
+
+		public void setIdExpediteur(int idExpediteur) {
+			this.idExpediteur = idExpediteur;
+		}
+
+		public int getIdDestinataire() {
+			return idDestinataire;
+		}
+
+		public void setIdDestinataire(int idDestinataire) {
+			this.idDestinataire = idDestinataire;
+		}
+
+		@Override
+		public String toString() {
+			return "Notification [idNotification=" + idNotification + ", message=" + message + ", dateEnvoi="
+					+ dateEnvoi + ", lu=" + lu + ", typeNotification=" + typeNotification + ", idExpediteur="
+					+ idExpediteur + ", idDestinataire=" + idDestinataire + "]";
+		}
+	    
+		
+	    
+}
---
+++ TypeNotification.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum TypeNotification {
+	 Alerte,Demande,Information
+}
