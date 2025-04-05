fd64fcf78421b52e713c925c028be58ec7464ebf
Sarahougour <155068819+Sarahougour@users.noreply.github.com>
2025-04-05 13:22:12 +0100

    Add files via upload
    la classe de la table MANY TO MANY etudiants_modules

---
+++ EtudiantModule.java
@@ -0,0 +1,35 @@
+package model;
+
+public class EtudiantModule {
+	private int idEtudiant;
+    private int idModule;
+	
+    public EtudiantModule(int idEtudiant, int idModule) {
+		super();
+		this.idEtudiant = idEtudiant;
+		this.idModule = idModule;
+	}
+
+	public int getIdEtudiant() {
+		return idEtudiant;
+	}
+
+	public void setIdEtudiant(int idEtudiant) {
+		this.idEtudiant = idEtudiant;
+	}
+
+	public int getIdModule() {
+		return idModule;
+	}
+
+	public void setIdModule(int idModule) {
+		this.idModule = idModule;
+	}
+
+	@Override
+	public String toString() {
+		return "EtudiantModule [idEtudiant=" + idEtudiant + ", idModule=" + idModule + "]";
+	}
+
+    
+}
