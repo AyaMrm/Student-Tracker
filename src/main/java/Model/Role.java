039632ea01ed03aed8eb1f1da1bb3c55010b712a
MrmAya <kahlimeriemaya@gmail.com>
2025-04-18 21:25:47 +0200

    ajout Role class

--- src/main/java/Model/Role.java
+++ src/main/java/Model/Role.java
@@ -1,12 +1,7 @@
 package Model;
 
-import java.math.BigDecimal;
-import java.sql.*;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
 public enum Role {
     ADMIN,
     ETUDIANT,
-    PROF}
+    PROF
+}
