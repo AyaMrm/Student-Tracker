bf81eaad42c2cdff42eadff1a8e8724df49f0c12
amiraber <155098066+amiraber@users.noreply.github.com>
2025-04-29 23:37:56 +0200

    structre

--- src/Model/JourSemaine.java
+++
@@ -1,6 +0,0 @@
-package Model;
-
-public enum JourSemaine {
-    Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche;
-
-}
--- src/Model/StatutPresence.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public enum StatutPresence {
-	Présent,
-    Absent,
-    Justifié
-}
